ef36bad07924f6ac2d349da4179d2713ab0c1210
Mappland <virusliye@gmail.com>
2023-12-22 06:43:03 +0800

    测试类更新

--- src/test/java/test_Jwt_Create.java
+++ src/test/java/test_Jwt_Create.java
@@ -1,7 +1,14 @@
+import org.mappland.ProException.JwtException;
+import org.mappland.function.Jwt;
+
 public class test_Jwt_Create {
     public static void main(String[] args) {
         // 测试生成JWT
-//        String token = Jwt.Jwt_generate("username"); // 1小时的有效期
-//        System.out.println("Generated JWT: " + token);
+        try{
+            String token = Jwt.Jwt_generate("username"); // 1小时的有效期
+            System.out.println("Generated JWT: " + token);
+        }catch (JwtException.CreateError error) {
+            System.out.println("创建错误");
+        }
     }
 }
