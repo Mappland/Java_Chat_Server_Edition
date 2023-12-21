781041f00a1967f6f12eec06901aca9b44e46cca
Mappland <virusliye@gmail.com>
2023-12-22 04:12:17 +0800

    用户创建请求及登录请求测试类

---
+++ src/test/java/test_User_Verify.java
@@ -0,0 +1,16 @@
+import com.google.gson.JsonObject;
+
+import function.*;
+
+
+public class test_User_Verify {
+
+    public static void main(String[] args) {
+        String jsonInputString = "{\"username\": \"user_3\", \"password\": \"exampsdflePassword\"}";
+
+        JsonObject result = Response_Client.get_response("/user_verify", "POST", jsonInputString);
+        if (result != null) {
+            System.out.println(result.toString());
+        }
+    }
+}
---
+++ src/test/java/test_User_create.java
@@ -0,0 +1,16 @@
+import com.google.gson.JsonObject;
+
+import function.Response_Client;
+
+
+public class test_User_create {
+
+    public static void main(String[] args) {
+        String jsonInputString = "{\"username\": \"user_3\", \"password\": \"examplePassword\"}";
+
+        JsonObject result = Response_Client.get_response("/user_create", "POST", jsonInputString);
+        if (result != null) {
+            System.out.println(result.toString());
+        }
+    }
+}
