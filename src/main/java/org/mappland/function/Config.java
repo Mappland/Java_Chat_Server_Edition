1d100ed3be29aa217cb0fb9e68506500552acb71
Mappland <virusliye@gmail.com>
2023-12-22 06:24:51 +0800

    新增配置文件功能

---
+++ src/main/java/org/mappland/function/Config.java
@@ -0,0 +1,47 @@
+package org.mappland.function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+
+public class Config {
+    private static final Logger logger = LoggerFactory.getLogger(Config.class);
+    public String jwt_key;
+    public int user_port;
+    public int chat_port;
+    public Config(){
+        try{
+            Yaml yaml = new Yaml();
+            FileInputStream inputStream = new FileInputStream("config/config.yaml");
+            Map<String, Object> config = yaml.load(inputStream);
+
+            if (config.containsKey("Server")) {
+                Map<String, Object> serverMap = (Map<String, Object>) config.get("Server");
+                if (serverMap.containsKey("KEY")) {
+                    Map<String, Object> map_key = (Map<String, Object>) serverMap.get("KEY");
+                    if (map_key.containsKey("Jwt")) {
+                        this.jwt_key = (String) map_key.get("Jwt");
+                    }
+                }
+                if (serverMap.containsKey("Port")) {
+                    Map<String, Object> map_key = (Map<String, Object>) serverMap.get("KEY");
+                    if (map_key.containsKey("User_Port")) {
+                        this.user_port = (int) map_key.get("User_Port");
+                    }
+                    if (map_key.containsKey("Chat_Port")) {
+                        this.chat_port = (int) map_key.get("Chat_Port");
+                    }
+                }
+            }
+            logger.info("配置文件读取成功");
+        }catch (IOException e){
+            logger.error("配置文件读取错误");
+        }
+    }
+}
+
+
